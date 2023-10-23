16caf57c80fddd6574c1275529fc430191dd3948
915-Misan-Teodora <72067734+915-Misan-Teodora@users.noreply.github.com>
2023-10-23 22:01:58 +0300

    repository skeleton

---
+++ persistancy/GlobalRepository.py
@@ -0,0 +1,31 @@
+import os
+import json 
+
+class GlobalRepository:
+    
+    """This class handles the management of the metadata of the dbmds from creation of databases, tables to indexes.
+    @context_database : refers to the database in use on the server from calls like (USE DATABASE x)
+    """
+
+    def create_database(database):
+        pass
+    
+    def drop_database(database_name):
+        pass
+    
+    def create_table(context_database, table):
+        pass
+    
+    def drop_table(context_database, table_name):
+        pass
+    
+    def create_index(context_database, index):
+        pass
+    
+    
+
+    
+    
+    
+    
+    
